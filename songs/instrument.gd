f154dfa3d209fa0dd91b16624f80647a1ecc249a
bemain <bemain.dev@gmail.com>
2024-12-29 18:40:42 +0100

    Implement  getter

--- songs/instrument.gd
+++ songs/instrument.gd
@@ -4,3 +4,7 @@ class_name Instrument
 @export var name: String
 
 @export var notes: Array[Note]
+
+
+func notes_in_beat(beat: int) -> Array[Note]:
+	return notes.filter(func (note): note.beat==beat)
--- songs/note.gd
+++ songs/note.gd
@@ -1,6 +1,8 @@
 extends Resource
 class_name Note
 
+@export var track: int
+
 @export var beat: int
 @export_range(0,1) var subbeat: float
 
