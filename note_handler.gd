9dba261935f490c4cfdb06b8f9d761a58ee6aa2a
TTBob <erik.k.lagerstedt@gmail.com>
2024-12-30 11:23:38 +0100

    Implement instrument track path following

--- note_handler.gd
+++ note_handler.gd
@@ -5,35 +5,37 @@ var instrument: Instrument
 
 @export var length: int = 500
 @export var beats: int = 4
+@export var width: int = 64
 
 var beat_length: float:
 	get: return length / beats
 var beat_duration: float:
 	get: return 60.0/song.bpm
-var note_speed: float: 
-	get: return beat_length / beat_duration
 
 var current_beat: int = 0
 
 var note = load("res://note.tscn")
 
+var path: Path2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	song = SongLoader.load_song("res://songs/jazz_swing.chrp")
-	print(song.title)
+	print("Playing: ", song.title)
 	instrument = song.instruments[0]
 	
+	path = $"Path2D"
+	
 	var beat_timer = Timer.new()
-	beat_timer.wait_time = 60.0/song.bpm
+	beat_timer.wait_time = beat_duration
 	beat_timer.one_shot = false
 	beat_timer.connect("timeout", new_beat)
 	add_child(beat_timer)
 	beat_timer.start()
 
 func new_beat() -> void:
-	print("Beat ", current_beat)
 	for n in instrument.notes_in_beat(current_beat):
-		print("New note on beat ", current_beat, " on track ", n.track, " with offset ", n.subbeat)
-		get_child(1).add_child(note.instantiate().init(note_speed, n.track, -beat_length * n.subbeat))
+		#print("New note on beat ", current_beat, " on track ", n.track, " with offset ", n.subbeat)
+		get_child(2).add_child(note.instantiate().init(path, beats*beat_duration, n.track, width, current_beat * beat_duration, -beat_duration * n.subbeat))
 	current_beat += 1
